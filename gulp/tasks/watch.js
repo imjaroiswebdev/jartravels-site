20d7ffa9ac5b5dc0bf510938bc617d0370b4e6c4
José Reyes <ja.reyes29@gmail.com>
2017-01-31 16:14:52 -0400

    Ya estan listos los estilos de Large Hero Block y el contenido introductorio.

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('watch', function () {
+    // En esta tarea se declaran todas las tareas que estarán corriendo
+    // cuando este funcionando $ gulp watch
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "app" // El metodo init recibe como argumento un objeto con las caracteristicas para funcionar
+        }
+    });
+
+    watch('./app/index.html', function () {
+        browserSync.reload(); // Recargar la página cada vez que se guarden cambios en index.html
+    });
+
+    watch('./app/assets/styles/**/*.css', function () {
+        gulp.start('cssInject'); // A través de cssInject se aplicará el procesamiento de PostCSS
+    });
+});
+
+gulp.task('cssInject', ['styles'], function () { // Se esta pasando como dependencia de la tarea cssInject, la tarea styles, para que aplique todo el procesamiento de PostCSS antes de proceder a inyectar los cambios en el CSS que se esta presentando
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
+});
